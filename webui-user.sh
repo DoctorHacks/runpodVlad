c79a5f2d0bbc4b3c30bab240037925a384834936
Brandon <131495413+DoctorHacks@users.noreply.github.com>
2023-05-29 12:59:30 -0400

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export COMMANDLINE_ARGS="--skip-torch --skip-requirements --skip-git --skip-update --skip-extensions --listen"
-install_dir="/workspace/automatic"
 venv_dir="/workspace/automatic/venv"
