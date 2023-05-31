30282ee29315433b33c9149d20e20a34de0286bb
Brandon <131495413+DoctorHacks@users.noreply.github.com>
2023-05-31 14:55:57 -0400

    Update relauncher.py

--- default-model-runtime/relauncher.py
+++ default-model-runtime/relauncher.py
@@ -3,13 +3,13 @@ import os, time
 n = 0
 while True:
     print('Relauncher: Launching...')
-    if n > 0:
-        print(f'\tRelaunch count: {n}')
     launch_string = "/workspace/automatic/webui.sh -f"
     if n == 0:
-        # 'y' input to download default model (never read if not needed)
-        os.system("echo 'y' | " + launch_string)
+        # On first launch, pass 'y' as user input to download default model
+        # If n == 0 from a restart, this "y" is never read
+        os.system("echo \"y\" | " + launch_string)
     else:
+        print(f'\tRelaunch count: {n}')
         os.system(launch_string)
     print('Relauncher: Process is ending. Relaunching in 2s...')
     n += 1
